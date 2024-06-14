f09c45b1cdea8ae8153214501954b7adb8f0df1a
Alexey-Voronin <voronin2@illinois.edu>
2024-06-14 16:21:45 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="phmg",
+    version="0.1",
+    packages=find_packages(),
+    description="Monolithic multigrid preconditioners for high-order Stokes systems.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Alexey Voronin",
+    author_email="voronin2@illinois.edu",
+    url="https://github.com/alexey-voronin/HighOrderStokesMG",
+    install_requires=[
+        "firedrake>=0.13.0",
+        "pandas>=1.4.0",
+    ],
+    package_data={},
+)
